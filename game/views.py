6dfc79a314ef755f40067036e4bbaedce23d4383
Egor Siropov <stafffsss@mail.ru>
2020-10-07 19:25:33 +0300

    ver 2

--- game/views.py
+++ game/views.py
@@ -1,7 +1,5 @@
-
 from django.shortcuts import render, redirect
 from random import randint
-
 from game.forms import GameForm
 from game.models import Player, Game, PlayerGameInfo
 
@@ -13,7 +11,6 @@ def show_home(request, *args, **kwargs):
     number = randint(0, 11)
     players = list(data_player.values())
     session = request.session.items()
-
 
     #если игрок уже завершил игру в сесcии
     try:
@@ -35,7 +32,6 @@ def show_home(request, *args, **kwargs):
             request.session['number'] = number
 
         else:
-
             player_id = players[-1]['id']
             if player_id == 1:
                 second_player = Player.objects.create(player_id=player_id+1)
@@ -48,7 +44,6 @@ def show_home(request, *args, **kwargs):
                 request.session['attempt'] = 0
 
             # для последующих игроко добавление DB и сохранения сессии
-
             if player_id % 2 == 0 and player_id > 1:
                 primary_player = player_id + 1
                 player = Player.objects.create(player_id=primary_player)
@@ -81,8 +76,6 @@ def show_home(request, *args, **kwargs):
         for item in data_player.values():
             if int(item['player_id']) == int(request.session['player']) and item['success_attempt'] != None:
                 request.session['gameover'] = True
-            
-
     else:
         user = 2
 
@@ -97,6 +90,7 @@ def show_home(request, *args, **kwargs):
 
     if request.method == 'POST':
         form = GameForm(request.POST)
+        print(form)
         if form.is_valid():
             request.session['attempt'] += 1
             example_number = request.POST['example_number']
@@ -104,19 +98,14 @@ def show_home(request, *args, **kwargs):
             if int(example_number) == int(number_result):
                 search_game = Game.objects.values()
                 for game in search_game:
-
                     if game['number'] == int(number_result):
                         game_id = game['id']
-
                         for player in PlayerGameInfo.objects.values():
-
                             if player['game_id'] == game_id and request.session['player'] != player['player_id']:
                                 first_player = data_player.get(player_id=player['player_id'])
                                 first_player.success_attempt = int(request.session['attempt'])
                                 first_player.save()
                                 request.session['gameover'] = True
-
-
             context = {
                 'form': form,
                 'id': user,
@@ -129,3 +118,9 @@ def show_home(request, *args, **kwargs):
         'home.html',
         context
         )
+
+
+
+
+
+
